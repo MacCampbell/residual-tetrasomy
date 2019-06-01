c529dff57e96ba99c1f423ecd4f2e95685ba103c
MacCampbell <campbellmatthewa@gmail.com>
2019-05-31 21:21:55 -0800

    Converted lastz script to be more general. Testing with subsetted chromos worked.

---
+++ 101-lastz.pl
@@ -0,0 +1,50 @@
+#! /usr/bin/perl -w
+# Started by Mac Campbell DrMacCampbell@gmail.com
+# April 17, 2019, Revision to GitHub on 05312019
+# The goal is to drive multiple lastz comparisons using
+# a "myPairs.txt" file that is tab-delimited and
+# gnu-parallel
+
+# Usage: ./101-lastz.pl processors genus-species path/to/genus-species-pairs
+# Example: ./101-lastz.pl 2 salmo-salar ./data/salmo-salar/salmo-salar-pairs.txt
+# How many simultaneous runs to do
+my $processors = shift;
+
+my $species = shift;
+
+# Make sure output dir exists
+`mkdir outputs/101/`;
+
+my @pairs;
+
+while(<>) {
+	chomp;
+	push (@pairs, $_);
+
+}
+
+print "The pairs I am going to compare are:\n";
+print join ("\n", @pairs);
+print "\n";
+
+#Prepare file for parallel
+open(OUTFILE, ">temp.txt") or die "Cannot open outfile";
+	foreach my $pair (@pairs) {
+	my @a=split(/\s/, $pair);
+	# Example with subsetting chromos
+	# printf OUTFILE "lastz ./data/$species/$species-$a[0].fasta\[1..2000000\] ./data/$species/$species-$a[1].fasta\[1..2000000\] --chain --gapped --gfextend --identity=75.0..100.0 --matchcount=100 --format=general --rdotplot=./outputs/101/$species-$a[0]-$a[1]-dotplot.txt --ambiguous=iupac --exact=20 > ./outputs/101/$species-$a[0]-$a[1].out\n";
+  # Full pairs
+   printf OUTFILE "lastz ./data/$species/$species-$a[0].fasta ./data/$species/$species-$a[1].fasta --chain --gapped --gfextend --identity=75.0..100.0 --matchcount=100 --format=general --rdotplot=./outputs/101/$species-$a[0]-$a[1]-dotplot.txt --ambiguous=iupac --exact=20 > ./outputs/101/$species-$a[0]-$a[1].out\n";
+
+	}
+    
+close(OUTFILE);
+
+`parallel -j $processors < temp.txt`;
+
+# Combine outfiles while excluding lines beginning with #
+`cat ./outputs/101/$species*.out | grep -v "#" > ./outputs/101/$species-101-lastz.result`;
+
+# Clean up 
+#`"yes" | rm *.out`;
+`"yes" | rm temp.txt`;
--- README.md
+++ README.md
@@ -5,11 +5,35 @@ For characterization of tetrasomic regions in salmonids
 To outline how to generate plots of similarities between tetrasomically pairing regions of salmonid genomes using any high-quality assembly.
 
 ## Files
-
-10X.X - Sequentially ordered files of what to do
+  * Should be designed so you can execute the files in the /magic-eight/ directory
+  * 10x.x-xxxxx.xx Sequentially ordered files of what to do
+  * These will basically run at first by using files in /data/ then putting outputs in /outputs/10x
+  * 10(x+1) should use /outputs/10x/ as inputs and output in /outputs/10(x+1)/ and so on
 
 ## File Structure
 
 /data/ genomes by sub directory (in .gitignore as /data/*)
 
 /outputs/ outputs (in .gitignore as /outputs/*)
+
+# Preparing Input Data
+## Creating a chromosome fasta file
+In order to work with NCBI genome files for our purposes, unscaffolded contigs need to be removed. We are storing the chromosome-only files on google drive. Here is an example of how to prepare one for _Salmo salar_.
+
+1. Download GCA_000233375.4_ICSASG_v2_genomic.fna  via the NCBI site
+2. Get the names of the chromosome sequences
+  * $ grep "ssa" GCA_000233375.4_ICSASG_v2_genomic.fna > names.txt
+  * $ xargs samtools faidx GCA_000233375.4_ICSASG_v2_genomic.fna < names.txt > salmo-salar-chroms.fasta
+3. This file should be placed in /data/salmo-salar/
+4. You can further split as needed like so:
+  * $ samtools faidx salmoChroms.fasta ssa17 > salmo-salar-ssa17.fasta
+  * $ samtools faidx salmoChroms.fasta ssa16 > salmo-salar-ssa16.fasta
+
+## Identifying centromeres and further splitting
+1. TBD - presumably with alignment to _Esox lucius_ or self
+
+# Running lastz and Generating Figures
+
+1. Dependencies: lastz, perl, Rscript, gnu-parallel
+
+
